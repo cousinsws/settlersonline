0291760df642c1f21fc38b1bfdccbdf00b47ccca
Sam Cousins <100026793@mvla.net>
2024-01-06 03:39:41 -0800

    joinGame refactor, value dots, value chip coloring

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -10,6 +10,7 @@ let amLeader;
 let players;
 
 let createdServer = false;
+let startedGame = false;
 
 //math consts
 const SQRT_3 = Math.sqrt(3);
@@ -17,6 +18,24 @@ const HALF_SQRT_3 = SQRT_3 / 2;
 const ONE_THIRD = 1/3;
 const TWO_THIRD = 2/3;
 const ROOT_THREE_OVER_THREE = SQRT_3/3;
+
+//visual parameters
+const SIZE_BUFFER = 1.03; //spent maybe three hours hassling with this multiplier only to decide it should probably just be 1 - keeping it for now?
+const VALUE_CHIP_SIZE = 2/9;
+
+const dicePermutations = {
+    2: 1,
+    3: 2,
+    4: 3,
+    5: 4,
+    6: 5,
+    7: 6,
+    8: 5,
+    9: 4,
+    10: 3,
+    11: 2,
+    12: 1
+}
 
 class Player {
     constructor(id, name, color, isBot, isLeader, isMe) {
@@ -131,7 +150,7 @@ function addBot(botChangeType) {
 
 async function createAndConnectServer() {
     if(createdServer) {
-        return;
+        return; //no spam for u
     }
     createdServer = true;
     console.log("CLICKED CREATE");
@@ -233,6 +252,10 @@ async function updatePlayerList() {
 }
 
 function startGame() {
+    if(startedGame) {
+        return; //no spam for u
+    }
+    startedGame = true;
     $.post("/queue/game/" + gameCode + "/start");
 }
 
@@ -247,44 +270,72 @@ async function joinGame(message) {
     const body = JSON.parse(message.body);
     const scenario = body.scenario;
     console.log("Recieved gamestart from " + body.gameCode + ":" + message.body);
-    const tilemap = body.tileMap;
-    let tiles = $('#tiles');
     const size = scenario === "THREE_FOUR" ? 12 : 9;
-    const sizeBuffer = 1.03; //spent maybe three hours hassling with this multiplier only to decide it should probably just be 1
+    drawTiles(body.tileMap, size);
+    drawVertices(body.landVertices, size);
+    drawPorts(body.ports, size);
+}
+
+function drawTiles(tilemap, size) {
+    let tiles = $('#tiles');
     tiles.empty();
     tilemap.forEach(pair => {
         const coordinate = pair.coordinate;
         const tile = pair.tile;
-        const [x, y] = toScreenCoordinates(coordinate, size, sizeBuffer); //tbh i got no idea why you dont use translatecoords here but issok!
-        tiles.append("<div class='tile' style=" +
-            "'width: " + size + "vh;height: " + 2*size + "vh; transform: translate(" + (x) + "vh, " + (y) + "vh) rotate(120deg) ;'" +
-            "><div class='tile-in1'><div class='tile-in2' style='background-image: url(" + toTileImage(tile.resource) + ")'>" +
-                (tile.resource != null ? ("<div class='value-chip' style='padding: " + (size/4.5) + "vh; box-sizing: border-box'>" +
-                    "<div class='value-icon'>" + tile.rollValue + "</div>" +
-                "</div>") : "") +
-            "</div></div></div>");
+        const [x, y] = toScreenCoordinates(coordinate, size); //tbh i got no idea why you dont use translatecoords here but issok!
+        tiles.append(getTile(tile, x, y, size));
     });
+}
+
+function getTile(tile, x, y, size) {
+    return  "<div class='tile' style='width: " + size + "vh;height: " + 2*size + "vh; transform: translate(" + (x) + "vh, " + (y) + "vh) rotate(120deg) ;'>" +
+                "<div class='tile-in1'>" +
+                    "<div class='tile-in2' style='background-image: url(" + toTileImage(tile.resource) + ")'>" +
+                        (tile.resource != null ? getValueChip(tile, size) : "") +
+                    "</div>" +
+                "</div>" +
+            "</div>";
+}
+
+function getValueChip(tile, size) {
+    return  "<div class='value-chip' style='padding: " + (size * VALUE_CHIP_SIZE) + "vh; box-sizing: border-box'>" +
+                "<div class='value-icon " + ((tile.rollValue === 6 || tile.rollValue === 8) ? "valuable" : "") + "'>" + //red if 6/8
+                    "<div class='value-icon-number'>" + tile.rollValue + "</div>" +
+                    "<div class='value-icon-dots'>" + getValueDots(tile.rollValue) + "</div>" +
+                "</div>" +
+             "</div>";
+}
+
+function getValueDots(rollValue) {
+    const perm = dicePermutations[rollValue];
+    const pad = (dicePermutations[6] - perm) / 2;
+    return "&nbsp;".repeat(pad) + "&centerdot;".repeat(perm) + "&nbsp;".repeat(pad); //&nbsp; is "non-breaking-space" so the padding doesnt get deleted by compiler (or smth idk)
+}
+
+function drawVertices(landVertices, size) {
     let vertices = $("#vertices");
     vertices.empty();
-    body.landVertices.forEach(vertex => {
-        const [sx, sy] = toScreenCoordinates(getVertexCoordinate(vertex.tileCoordinate, vertex.direction), size, sizeBuffer);
-        const [x, y] = toTranslateCoordinates(sx, sy, sizeBuffer, size);
+    landVertices.forEach(vertex => {
+        const [sx, sy] = toScreenCoordinates(getVertexCoordinate(vertex.tileCoordinate, vertex.direction), size);
+        const [x, y] = toTranslateCoordinates(sx, sy, size);
         let vDiv = $("<div class='vertex' id='lastV' style='transform: translate(" + x + "," + y + ")'></div>");
         vDiv.click(function() {onVertexClick(vertex);});
         vDiv.appendTo($('#vertices'));
         const v = $("#lastV");
         vertexMap.set(JSON.stringify(vertex), new Vertex(v[0], v, sx, sy));
         v.removeAttr('id');
-        //TODO draw face values
     });
-    body.ports.forEach(coordinateport => {
+}
+
+function drawPorts(ports, size) {
+    ports.forEach(coordinateport => {
         const anchor = coordinateport.anchor;
         anchor.r++;
         const port = coordinateport.port;
         const resource = port.resource;
         const portVertices = port.vertices;
-        const [sx, sy] = toScreenCoordinates(anchor, size, sizeBuffer);
-        const [x, y] = toTranslateCoordinates(sx, sy, sizeBuffer, size);
+        const [sx, sy] = toScreenCoordinates(anchor, size);
+        const [x, y] = toTranslateCoordinates(sx, sy, size);
         let portDiv = $("<div class='port' style='background-image: url(" + toTileImage(resource) + "); transform: translate(" + x + "," + y + ")'></div>");
         portDiv.appendTo($('#ports'));
         const gangwayLenFactor = 2/5;
@@ -293,11 +344,11 @@ async function joinGame(message) {
         portVertices.forEach(vertex => { // (there are 2)
             //x, y, sx, and sy still in scope!
             const vcoord = getVertexCoordinate(vertex.tileCoordinate, vertex.direction);
-            const [vsx, vsy] = toScreenCoordinates({q: (anchor.q + vcoord.q)/2, r: (anchor.r + vcoord.r)/2}, size, sizeBuffer); //mean position btwn anchor and portvertex
+            const [vsx, vsy] = toScreenCoordinates({q: (anchor.q + vcoord.q)/2, r: (anchor.r + vcoord.r)/2}, size); //mean position btwn anchor and portvertex
             const [dsx, dsy] = [vsx - sx, vsy - sy];
             const theta = Math.atan(dsx/dsy); //math!
             // using our own version of toTranslateCoordinates here - need to move up, not down
-            const [x, y] = toTranslateCoordinates(vsx, vsy, sizeBuffer, size);
+            const [x, y] = toTranslateCoordinates(vsx, vsy, size);
             $("<div class='gangway' style='width: " + w + "vh; height: " + (h * gangwayLenFactor) + "vh; " +
                 "transform: translate(" + x + ", calc(" + y + ")) rotate(" + (-theta) + "rad)'></div>")
                 .appendTo($('#ports')); //dont need to save it im never touching these again
@@ -317,14 +368,14 @@ function onVertexClick(vertex) {
     console.log("Vertex @ " + JSON.stringify(vertex.tileCoordinate) + " to " + vertex.direction + " clicked!"); //TODO
 }
 
-function toScreenCoordinates(coordinate, size, sizeBuffer) {
+function toScreenCoordinates(coordinate, size) {
     const q = coordinate.q;
     const r = coordinate.r;
-    return [(sizeBuffer * (q*size + r*size/2)) - size/2, sizeBuffer * (r*size*HALF_SQRT_3) - size];
+    return [(SIZE_BUFFER * (q*size + r*size/2)) - size/2, SIZE_BUFFER * (r*size*HALF_SQRT_3) - size];
 }
 
-function toTranslateCoordinates(x, y, sizeBuffer, size) {
-    return ["calc(-50% + " + (x - (sizeBuffer-1)*size / 2) + "vh)", "calc(25% + " + (y - (sizeBuffer-1)*size) + "vh)"];
+function toTranslateCoordinates(x, y, size) {
+    return ["calc(-50% + " + (x - (SIZE_BUFFER-1)*size / 2) + "vh)", "calc(25% + " + (y - (SIZE_BUFFER-1)*size) + "vh)"];
 }
 
 function toTileImage(resource) {
