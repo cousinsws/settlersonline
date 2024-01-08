b4099e4af01043e6d55119668797d21f8526eefe
Sam Cousins <100026793@mvla.net>
2024-01-08 03:38:52 -0800

    player; enemy infocards, update some icons. (basically) ready for func

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -7,7 +7,7 @@ let sessionID;
 let gameCode;
 let myProfile;
 let amLeader;
-let players;
+let users;
 
 let createdServer = false;
 let startedGame = false;
@@ -22,6 +22,15 @@ const ROOT_THREE_OVER_THREE = SQRT_3/3;
 //visual parameters
 const SIZE_BUFFER = 1.03; //spent maybe three hours hassling with this multiplier only to decide it should probably just be 1 - keeping it for now?
 const VALUE_CHIP_SIZE = 2/9;
+const COLOR_RED = "rgba(175, 13, 13, 100%)";
+const COLOR_BLUE = "rgba(14, 71, 168, 100%)";
+const COLOR_WHITE = "rgb(238,206,176)";
+const COLOR_ORANGE = "rgba(222, 115, 31, 100%)";
+const COLOR_YELLOW = "rgba(217, 176, 14, 100%)";
+const COLOR_GREEN = "rgb(20,107,29)";
+const COLOR_PURPLE = "rgba(115, 12, 164, 100%)";
+const COLOR_BLACK = "rgba(35, 12, 3, 100%)";
+const COLOR_BLACK_DISPLAY = "rgb(93,93,91)";
 
 const dicePermutations = {
     2: 1,
@@ -38,8 +47,7 @@ const dicePermutations = {
 }
 
 class Player {
-    constructor(id, name, color, isBot, isLeader, isMe) {
-        this.id = id;
+    constructor(name, color, isBot, isLeader, isMe) {
         this.name = name;
         this.color = color;
         this.isBot = isBot;
@@ -58,6 +66,7 @@ class Vertex {
 }
 
 let vertexMap = new Map();
+let players = new Map();
 
 function getCookie(cname) {
     let name = cname + "=";
@@ -217,9 +226,8 @@ async function updatePlayerList() {
     const playersResponse = await fetch('/queue/game/' + gameCode + '/getPlayers');
     console.log(playersResponse);
     let playerList = await playersResponse.json();
-    console.log(players);
     $('#connected-players').empty();
-    players = [];
+    users = [];
     playerList.sort().forEach(pl => {
         console.log(pl);
         let color = pl.color;
@@ -232,10 +240,10 @@ async function updatePlayerList() {
             myProfile = pl;
             amLeader = isLeader;
         }
-        players.push(new Player(playerID, name, color, isBot, isLeader, isMe));
+        users.push(new Player(name, color, isBot, isLeader, isMe));
         $('#connected-players').append("<div class='connected-player'>" + (isLeader ? "&#9733;" : "") + name + (isMe ? " (You)" : "") + (isBot ? " (BOT)" : "") + "</div>");
     })
-    let numConnected = players.length;
+    let numConnected = users.length;
     if(amLeader) {
         $("#bot-panel").show();
         if(numConnected >= 3) {
@@ -270,10 +278,96 @@ async function joinGame(message) {
     const body = JSON.parse(message.body);
     const scenario = body.scenario;
     console.log("Recieved gamestart from " + body.gameCode + ":" + message.body);
-    const size = scenario === "THREE_FOUR" ? 12 : 9;
+    const size = scenario === "THREE_FOUR" ? 11 : 8;
     drawTiles(body.tileMap, size);
     drawVertices(body.landVertices, size);
     drawPorts(body.ports, size);
+    drawPlayers(body.playerOrder);
+}
+
+function drawPlayers(playerOrder) {
+    playerOrder.forEach(pl => {
+        console.log(pl);
+        let color = pl.color;
+        let name = pl.name;
+        let isBot = pl.type === "ROBOT";
+        let isLeader = pl.isLeader;
+        let playerID = pl.ID;
+        let isMe = playerID === sessionID;
+        if(isMe) {
+            myProfile = pl;
+            amLeader = isLeader;
+        }
+        const plO = new Player(name, color, isBot, isLeader, isMe);
+        if(isMe) {
+            drawHero();
+            return; //continue;
+        }
+        $('#player-banners').append(getEnemyBanner(plO, players.size));
+    });
+}
+
+function getEnemyBanner(plO, orderPlace) {
+    return               "<div class='polygon-banner' style='background-color: " + getPlayerColorStyle(plO.color) +";'>\n" +
+        "                    <div class='name-display enemy'>\n" +
+        "                        <div id='enemy" + orderPlace + "-score-value' class='victory-point-star'>0</div>\n" +
+        "                        <div id='enemy" + orderPlace + "-name' class='name'>" + (plO.name + (plO.isBot ? " (BOT)" : "")) +"</div>\n" +
+        "                    </div>\n" +
+        "                    <div style='display: flex; flex-direction: row; justify-content: center;'>\n" +
+        "                        <div class='horiz-info'>\n" +
+        "                            <div class='enemy-info-card-icon'></div>\n" +
+        "                            <div id='enemy" + orderPlace + "-info-card-value'>0</div>\n" +
+        "                        </div>\n" +
+        "                        <div class='horiz-info'>\n" +
+        "                            <div class='enemy-info-dev-icon'></div>\n" +
+        "                            <div id='enemy" + orderPlace + "-info-dev-value'>0</div>\n" +
+        "                        </div>\n" +
+        "                        <div class='horiz-info'>\n" +
+        "                            <div id='enemy" + orderPlace + "-info-lr-icon' style='filter: brightness(25%)' class='enemy info-lr-icon'></div>\n" +
+        "                            <div id='enemy" + orderPlace + "-info-lr-value'>0</div>\n" +
+        "                        </div>\n" +
+        "                        <div class='horiz-info'>\n" +
+        "                            <div id='enemy" + orderPlace + "-info-la-icon' style='filter: brightness(25%)' class='enemy info-la-icon'></div>\n" +
+        "                            <div id='enemy" + orderPlace + "-info-la-value'>0</div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>"
+}
+
+function getPlayerColorStyle(color) {
+    switch(color) {
+        case "RED": return COLOR_RED;
+        case "BLUE": return COLOR_BLUE;
+        case "WHITE": return COLOR_WHITE;
+        case "ORANGE": return COLOR_ORANGE;
+        case "YELLOW": return COLOR_YELLOW;
+        case "GREEN": return COLOR_GREEN;
+        case "PURPLE": return COLOR_PURPLE;
+        case "BLACK": return COLOR_BLACK_DISPLAY;
+    }
+    return COLOR_RED;
+}
+
+function getPieceColorStyle(color) {
+    switch(color) {
+        case "RED": return COLOR_RED;
+        case "BLUE": return COLOR_BLUE;
+        case "WHITE": return COLOR_WHITE;
+        case "ORANGE": return COLOR_ORANGE;
+        case "YELLOW": return COLOR_YELLOW;
+        case "GREEN": return COLOR_GREEN;
+        case "PURPLE": return COLOR_PURPLE;
+        case "BLACK": return COLOR_BLACK;
+    }
+    return COLOR_RED;
+}
+
+function drawHero() { //hero profile in myProfile global
+    //most of it is raw html, just gotta set name
+    $("#hero-name").html(myProfile.name);
+    $("#hero-section").css("background-color", getPlayerColorStyle(myProfile.color));
+    $("#hero-lr-icon").css("filter", "brightness(25%)");
+    $("#hero-la-icon").css("filter", "brightness(25%)");
 }
 
 function drawTiles(tilemap, size) {
